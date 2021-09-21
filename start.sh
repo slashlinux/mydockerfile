acbfaaecf24d8000bb6b0c0d5f5c4cef137cac57
slashlinux <32353453+slashlinux@users.noreply.github.com>
2021-09-21 19:20:17 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update nginx to match worker_processes to no. of cpu's
+procs=$(cat /proc/cpuinfo | grep processor | wc -l)
+sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
+
+
+# Start supervisord and services
+/usr/local/bin/supervisord -n -c /etc/supervisord.conf
+/usr/sbin/php-fpm7.0 -D
